23fabca04c96a38563a31dc7c3eb0a7f1741244d
pratikn <pratikway.90@gmail.com>
2017-04-17 13:40:59 +0530

    fixing issues

---
+++ src/app/_const/globals.ts
@@ -0,0 +1,8 @@
+export class Globals{
+    //serviceurl : string = "http://localhost:8081/goyoapi/";
+    serviceurl : string = "http://35.154.27.42:8081/goyoapi/";
+
+    //serviceurl : string = "http://localhost:27301/ServiceERP.svc/JSON/";
+
+    // serviceurl : string = "http://192.168.1.104/erpservice/ServiceERP.svc/JSON/";
+}
