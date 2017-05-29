b929ea1ea3362c7f452c6383adeeff3a0ab198ad
llc <pratikway.90@gmail.com>
2017-05-29 16:47:51 +0530

    new order dashboard

---
+++ src/app/modules/murchant/orderdashboard/index.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SharedComponentModule } from '../../../_shared/sharedcomp.module';
+
+import { ViewComponent } from './view/view.comp';
+import { DataGridModule } from './datagrid';
+
+import { OrderService } from '../../../_services/order/ord-service';
+
+import { LazyLoadEvent, DataTableModule, SelectButtonModule, GMapModule } from 'primeng/primeng';
+
+export const routes = [
+  {
+    path: '', children: [
+      { path: '', component: ViewComponent },
+    ]
+  },
+];
+
+@NgModule({
+  declarations: [
+    ViewComponent
+  ],
+
+  imports: [
+    CommonModule, FormsModule,
+     SharedComponentModule,
+      RouterModule.forChild(routes), 
+      DataTableModule,
+     SelectButtonModule, GMapModule,
+    DataGridModule
+  ],
+
+  providers: [OrderService]
+})
+
+export class OrderDashboardModule {
+  public static routes = routes;
+}
