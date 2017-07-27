4563615e1d9023c5f32cdba31891d45a4165af2c
vivekpandey12 <vivek.pandey5188@gmail.com>
2017-07-28 02:15:46 +0530

    all work is committed

--- src/app/modules/reports/trips/rpttrips.comp.ts
+++ src/app/modules/reports/trips/rpttrips.comp.ts
@@ -21,26 +21,11 @@ export class TripsReportsComponent implements OnInit, OnDestroy {
     _enttdetails: any = [];
 
     monthDT: any = [];
-    tripsDT: any = [];
-    
     monthname: string = "";
 
+    tripsDT: any = [];
+
     doc = new jsPDF();
-
-    specialElementHandlers = {
-        '#editor': function (element, renderer) {
-            return true;
-        }
-    };
-
-    standardDT: any = [];
-    standard: string = "";
-
-    autoPassengerDT: any = [];
-    psngrid: number = 0;
-    psngrname: string = "";
-
-    passengerDT: any = [];
 
     @ViewChild('passenger') passenger: ElementRef;
 
@@ -76,8 +61,25 @@ export class TripsReportsComponent implements OnInit, OnDestroy {
 
     // Export
 
+    exportToExcel(e, table, name) {
+        console.log(e);
+
+        let uri = 'data:application/vnd.ms-excel;base64,',
+            template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--><meta http-equiv="content-type" content="text/plain; charset=UTF-8"/></head><body><table>{table}</table></body></html>',
+            base64 = function (s) { return window.btoa(decodeURI(decodeURIComponent(s))) },
+            format = function (s, c) {
+                return s.replace(/{(\w+)}/g, function (m, p) { return c[p]; })
+            }
+
+        if (!table.nodeType) table = document.getElementById(table)
+        var ctx = { worksheet: name || 'Worksheet', table: table.innerHTML }
+        window.location.href = uri + base64(format(template, ctx));
+
+        return false;
+    }
+
     public exportToCSV() {
-        new Angular2Csv(this.passengerDT, 'PassengerReports', { "showLabels": true });
+        new Angular2Csv(this.tripsDT, 'TripsReports', { "showLabels": true });
     }
 
     public exportToPDF() {
@@ -88,7 +90,7 @@ export class TripsReportsComponent implements OnInit, OnDestroy {
         };
 
         pdf.addHTML(this.passenger.nativeElement, 0, 0, options, () => {
-            pdf.save("PassengerReports.pdf");
+            pdf.save("TripsReports.pdf");
         });
     }
 
