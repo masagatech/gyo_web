105412cb9d07c8d744a7589ff3cad2e391ae426c
vivekpandey12 <vivek.pandey5188@gmail.com>
2017-07-28 17:13:42 +0530

    all work is committed

--- src/app/_const/globals.ts
+++ src/app/_const/globals.ts
@@ -1,17 +1,15 @@
 import { Cookie } from 'ng2-cookies/ng2-cookies';
 
 export class Globals {
-    serviceurl: string = "http://localhost:8082/goyoapi/";
-    uploadurl: string = "http://localhost:8082/images/";
-    filepath: string = "www\\uploads\\";
+    // serviceurl: string = "http://localhost:8082/goyoapi/";
+    // uploadurl: string = "http://localhost:8082/images/";
+    // filepath: string = "www\\uploads\\";
+    // static socketurl: string = "http://localhost:8082/";
 
-    static socketurl: string = "http://localhost:8082/";
-
-    // serviceurl: string = "http://track.goyo.in:8082/goyoapi/";
-    // uploadurl: string = "http://track.goyo.in:8082/images/";
-    // filepath: string = "www/uploads/";
-
-    // static socketurl: string = "http://track.goyo.in:8082/";
+    serviceurl: string = "http://track.goyo.in:8082/goyoapi/";
+    uploadurl: string = "http://track.goyo.in:8082/images/";
+    filepath: string = "www/uploads/";
+    static socketurl: string = "http://track.goyo.in:8082/";
 
     otherurl: string = "http://35.154.27.42:8081/goyoapi/";
 
