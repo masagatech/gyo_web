be6a1e14af3840b7ad5fc4734f3f848c76a6dd3a
llc <pratikway.90@gmail.com>
2017-11-17 13:49:00 +0530

    v

--- src/app/_const/globals.ts
+++ src/app/_const/globals.ts
@@ -2,12 +2,29 @@ import { Cookie } from 'ng2-cookies/ng2-cookies';
 
 export class Globals {
     static erproute: string = "erp/"
-    photoid : 29;
+    photoid: 29;
 
-    serviceurl: string = "http://localhost:8082/goyoapi/";
-    uploadurl: string = "http://localhost:8082/images/";
-    filepath: string = "www\\uploads\\";
-    static socketurl: string = "http://localhost:8082/";
+    // serviceurl: string = "http://localhost:8082/goyoapi/";
+    // trackurl: string = "http://localhost:6979/goyoapi/";
+
+    // uploadurl: string = "http://localhost:8082/images/";
+    // filepath: string = "www\\uploads\\";
+    // static socketurl: string = "http://localhost:6979/";
+
+    serviceurl: string = "http://track.goyo.in:8082/goyoapi/";
+    uploadurl: string = "http://track.goyo.in:8082/images/";
+    filepath: string = "www/uploads/";
+    static socketurl: string = "http://track.goyo.in:8082/";
+    // trackurl: string = "http://35.154.114.229:6979/goyoapi/";
+
+    static socketurl_trk: string = "http://35.154.114.229:6979/";
+    trackurl_trk: string = "http://35.154.114.229:6979/goyoapi/";
+
+
+    // serviceurl: string = "http://school.goyo.in:8082/goyoapi/";
+    // uploadurl: string = "http://school.goyo.in:8082/images/";
+    // filepath: string = "www/uploads/";
+    // static socketurl: string = "http://school.goyo.in:8082/";
 
     // serviceurl: string = "http://track.goyo.in:8082/goyoapi/";
     // uploadurl: string = "http://track.goyo.in:8082/images/";
