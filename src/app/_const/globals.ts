17a76d46386412bd35090c732e1bf7701c70e97c
vivekpandey12 <vivek.pandey5188@gmail.com>
2017-11-17 18:44:27 +0530

    committed

--- src/app/_const/globals.ts
+++ src/app/_const/globals.ts
@@ -2,44 +2,33 @@ import { Cookie } from 'ng2-cookies/ng2-cookies';
 
 export class Globals {
     static erproute: string = "erp/";
-    photoid: 29;
+    photoid: number = 29;
+    xlsid: number = 24;
 
-    serviceurl: string = "http://localhost:8082/goyoapi/";
-    uploadurl: string = "http://localhost:8082/images/";
-    static socketurl: string = "http://localhost:8082/";
-    filepath: string = "www\\uploads\\";
-    xlsfilepath: string = "www\\uploads\\exceluploads\\";
+    static weburl: string = window.location.hostname;
 
-    // serviceurl: string = "http://school.goyo.in:8082/goyoapi/";
-    // uploadurl: string = "http://school.goyo.in:8082/images/";
-    // static socketurl: string = "http://school.goyo.in:8082/";
-
-    // trackurl: string = "http://localhost:6979/goyoapi/";
-
-    // uploadurl: string = "http://localhost:8082/images/";
+    serviceurl: string = "http://" + Globals.weburl + ":8082/goyoapi/";
+    uploadurl: string = "http://" + Globals.weburl + ":8082/images/";
+    static socketurl: string = "http://" + Globals.weburl + ":8082/";
+    
     // filepath: string = "www\\uploads\\";
-    // static socketurl: string = "http://localhost:6979/";
-
-    // serviceurl: string = "http://track.goyo.in:8082/goyoapi/";
-    // uploadurl: string = "http://track.goyo.in:8082/images/";
-    // filepath: string = "www/uploads/";
-    // static socketurl: string = "http://track.goyo.in:8082/";
-    // trackurl: string = "http://35.154.114.229:6979/goyoapi/";
+    // xlsfilepath: string = "www\\uploads\\exceluploads\\";
+    
+    filepath: string = "www/uploads/";
+    xlsfilepath: string = "www/uploads/exceluploads/";
 
     static socketurl_trk: string = "http://35.154.114.229:6979/";
     trackurl_trk: string = "http://35.154.114.229:6979/goyoapi/";
 
+    otherurl: string = "http://35.154.27.42:8081/goyoapi/";
+
+    // serviceurl: string = "http://localhost:8082/goyoapi/";
+    // uploadurl: string = "http://localhost:8082/images/";
+    // static socketurl: string = "http://localhost:8082/";
 
     // serviceurl: string = "http://school.goyo.in:8082/goyoapi/";
     // uploadurl: string = "http://school.goyo.in:8082/images/";
-    // filepath: string = "www/uploads/";
     // static socketurl: string = "http://school.goyo.in:8082/";
-
-
-    // filepath: string = "www/uploads/";
-    // xlsfilepath: string = "www/uploads/exceluploads/";
-
-    otherurl: string = "http://35.154.27.42:8081/goyoapi/";
 
     public static getWSDetails() {
         let _wsdetails = Cookie.get("_schwsdetails_");
