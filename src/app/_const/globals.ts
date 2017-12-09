e958e3a200bf73b53aa5b61357e9261c35dfc10a
llc <pratikway.90@gmail.com>
2017-12-09 12:25:09 +0530

    committed

--- src/app/_const/globals.ts
+++ src/app/_const/globals.ts
@@ -5,6 +5,8 @@ export class Globals {
     photoid: number = 29;
     xlsid: number = 24;
 
+    // static weburl: string = "school.goyo.in";
+    // static weburl: string = "track.goyo.in";
     static weburl: string = window.location.hostname;
 
     serviceurl: string = "http://" + Globals.weburl + ":8082/goyoapi/";
@@ -22,13 +24,7 @@ export class Globals {
 
     otherurl: string = "http://35.154.27.42:8081/goyoapi/";
 
-    // serviceurl: string = "http://localhost:8082/goyoapi/";
-    // uploadurl: string = "http://localhost:8082/images/";
-    // static socketurl: string = "http://localhost:8082/";
-
-    // serviceurl: string = "http://school.goyo.in:8082/goyoapi/";
-    // uploadurl: string = "http://school.goyo.in:8082/images/";
-    // static socketurl: string = "http://school.goyo.in:8082/";
+    // Functions
 
     public static getWSDetails() {
         let _wsdetails = Cookie.get("_schwsdetails_");
